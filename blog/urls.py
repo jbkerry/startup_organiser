cb79defd6564159f6145ddf55611c025324d772b
Jon Kerry <jonbkerry@gmail.com>
2018-10-07 19:16:22 +0100

    End of Chapter 5 : Added a CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
-from .views import post_detail, post_list
+from .views import PostList, post_detail
 
 urlpatterns = [
-    url(r'^$', post_list, name='blog_post_list'),
+    url(r'^$', PostList.as_view(), name='blog_post_list'),
     url(
         r'^(?P<year>\d{4})/'
         r'(?P<month>\d{1,2})/'
         r'(?P<slug>[\w\-]+)/$',
         post_detail,
         name='blog_post_detail'
-    )
+    ),
 ]
