de1b72d74217d3f21d1ecbaf8970a08530df2f62
Jon Kerry <jonbkerry@gmail.com>
2018-10-07 18:27:50 +0100

    Chapter 5 : fully working function views for all templates

--- organizer/views.py
+++ organizer/views.py
@@ -1,13 +1,21 @@
-from django.http.response import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
-from .models import Tag
+from .models import Startup, Tag
 
 
-def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template(
-        'organizer/tag_list.html'
-    )
-    output = template.render({'tag_list': tag_list})
-    return HttpResponse(output)
+def startup_list(request):
+    return render(request, 'organizer/startup_list.html', {'startup_list': Startup.objects.all()})
+
+
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    return render(request, 'organizer/startup_detail.html', {'startup': startup})
+
+
+def tag_list(request):
+    return render(request, 'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(request, 'organizer/tag_detail.html', {'tag': tag})
